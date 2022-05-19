d21541d0ce25fb78b33acd5e6df284259bd3252c
juaninicolai <juan0719@stud.kea.dk>
2022-05-19 14:58:06 +0200

    added redux

--- Auth/LoginScreen.js
+++ Auth/LoginScreen.js
@@ -1,83 +1,89 @@
+/** @format */
+
 import { Input, Button } from "@rneui/base";
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
+import { useSelector, useDispatch } from "react-redux";
+import { setEmail, setPass } from "../Redux/Actions";
 
 const LoginScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const { email, password } = useSelector(state => state.userReducer);
+	const dispatch = useDispatch();
 
-  const signIn = () => {
-    signInWithEmailAndPassword(auth, email, password).catch(error => {
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
-  };
+	const signIn = () => {
+		signInWithEmailAndPassword(auth, email, password).catch(error => {
+			const errorMessage = error.message;
+			alert(errorMessage);
+		});
+	};
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, user => {
-      if (user) {
-        navigation.replace("Overview");
-      } else {
-        navigation.canGoBack() && navigation.popToTop();
-      }
-    });
-    return unsubscribe;
-  }, []);
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, user => {
+			if (user) {
+				navigation.replace("Overview");
+			} else {
+				navigation.canGoBack() && navigation.popToTop();
+			}
+		});
+		return unsubscribe;
+	}, []);
 
-  return (
-    <View style={styles.parentContainer}>
-    <View style={styles.container}>
-      <Input
-        placeholder="Enter your email"
-        label="Email"
-        leftIcon={{ type: "material", name: "email" }}
-        value={email}
-        onChangeText={text => setEmail(text)}
-      />
-      <Input
-        placeholder="Enter your password"
-        label="Password"
-        leftIcon={{ type: "material", name: "lock" }}
-        value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry
-      />
-      <Button title="Sign in" style={styles.button} onPress={signIn} />
-      <Button
-        title="Register"
-        style={styles.button}
-        onPress={() => navigation.navigate("Register")}
-      />
-      <Button
-        title="Forgot password"
-        style={styles.button}
-        onPress={() => navigation.navigate("Forgot Password")}
-      ></Button>
-      <Image style={{marginTop: 50}}
-      source={require("../assets/cbs_logo.png")}></Image>
-    </View>
-    </View>
-  );
+	return (
+		<View style={styles.parentContainer}>
+			<View style={styles.container}>
+				<Input
+					placeholder="Enter your email"
+					label="Email"
+					leftIcon={{ type: "material", name: "email" }}
+					value={email}
+					onChangeText={text => dispatch(setEmail(text))}
+				/>
+				<Input
+					placeholder="Enter your password"
+					label="Password"
+					leftIcon={{ type: "material", name: "lock" }}
+					value={password}
+					onChangeText={text => dispatch(setPass(text))}
+					secureTextEntry
+				/>
+				<Button title="Sign in" style={styles.button} onPress={signIn} />
+				<Button
+					title="Register"
+					style={styles.button}
+					onPress={() => navigation.navigate("Register")}
+				/>
+				<Button
+					title="Forgot password"
+					style={styles.button}
+					onPress={() => navigation.navigate("Forgot Password")}
+				></Button>
+				<Image
+					style={{ marginTop: 50 }}
+					source={require("../assets/cbs_logo.png")}
+				></Image>
+			</View>
+		</View>
+	);
 };
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-  button: {
-    width: 200,
-    marginTop: 10,
-  },
-  container: {
-    marginTop: 60,
-    flex: 2,
-    alignItems: "center",
-    padding: 10,
-    backgroundColor: "#FFFFFF",
-  },
-  parentContainer: {
-    flex: 1,
-    backgroundColor: "#FFFFFF"
-  }
+	button: {
+		width: 200,
+		marginTop: 10,
+	},
+	container: {
+		marginTop: 60,
+		flex: 2,
+		alignItems: "center",
+		padding: 10,
+		backgroundColor: "#FFFFFF",
+	},
+	parentContainer: {
+		flex: 1,
+		backgroundColor: "#FFFFFF",
+	},
 });
--- Auth/RegisterScreen.js
+++ Auth/RegisterScreen.js
@@ -1,94 +1,97 @@
+/** @format */
+
 import { Input, Button } from "@rneui/base";
 import { useState } from "react";
 import { View, StyleSheet, Alert, Image } from "react-native";
 import { auth } from "../config/firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useSelector, useDispatch } from "react-redux";
+import { setEmail, setName, setPhoto, setPass } from "../Redux/Actions";
 
 const RegisterScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [imageURL, setImageURL] = useState("");
+	const { email, name, password, photoURL } = useSelector(
+		state => state.userReducer
+	);
+	const dispatch = useDispatch();
 
-  const register = () => {
-    if (email !== "" && password !== "") {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then(res => {
-          updateProfile(res.user, {
-            displayName: name,
-            photoURL: imageURL
-              ? imageURL
-              : "https://example.com/jane-q-user/profile.jpg",
-          })
-            .then(() => {
-              // Profile updated!
-            })
-            .catch(error => {
-              // An error occurred
-            });
-        })
-        .catch(err => Alert.alert("signup error", err.message));
-      navigation.popToTop();
-    }
-  };
+	const register = () => {
+		if (email !== "" && password !== "") {
+			createUserWithEmailAndPassword(auth, email, password)
+				.then(res => {
+					updateProfile(res.user, {
+						displayName: name,
+						photoURL: photoURL
+							? photoURL
+							: "https://example.com/jane-q-user/profile.jpg",
+					})
+						.then(() => {
+							// Profile updated!
+						})
+						.catch(error => {
+							// An error occurred
+						});
+				})
+				.catch(err => Alert.alert("signup error", err.message));
+			navigation.popToTop();
+		}
+	};
 
-  return (
-    <View style={styles.parentContainer}>
-    <View style={styles.container}>
-      <Input
-        placeholder="Enter your name"
-        label="Name"
-        leftIcon={{ type: "material", name: "badge" }}
-        value={name}
-        onChangeText={text => setName(text)}
-      />
-      <Input
-        placeholder="Enter your email"
-        label="Email"
-        leftIcon={{ type: "material", name: "email" }}
-        value={email}
-        onChangeText={text => setEmail(text)}
-      />
-      <Input
-        placeholder="Enter your password"
-        label="Password"
-        leftIcon={{ type: "material", name: "lock" }}
-        value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry
-      />
-      <Input
-        placeholder="Enter your image URL"
-        label="Profile Picture"
-        leftIcon={{ type: "material", name: "face" }}
-        value={imageURL}
-        onChangeText={text => setImageURL(text)}
-      />
+	return (
+		<View style={styles.parentContainer}>
+			<View style={styles.container}>
+				<Input
+					placeholder="Enter your name"
+					label="Name"
+					leftIcon={{ type: "material", name: "badge" }}
+					value={name}
+					onChangeText={text => dispatch(setName(text))}
+				/>
+				<Input
+					placeholder="Enter your email"
+					label="Email"
+					leftIcon={{ type: "material", name: "email" }}
+					value={email}
+					onChangeText={text => dispatch(setEmail(text))}
+				/>
+				<Input
+					placeholder="Enter your password"
+					label="Password"
+					leftIcon={{ type: "material", name: "lock" }}
+					value={password}
+					onChangeText={text => dispatch(setPass(text))}
+					secureTextEntry
+				/>
+				<Input
+					placeholder="Enter your image URL"
+					label="Profile Picture"
+					leftIcon={{ type: "material", name: "face" }}
+					value={photoURL}
+					onChangeText={text => dispatch(setPhoto(text))}
+				/>
 
-      <Button title="Register" style={styles.button} onPress={register} />
-      <Image source={require("../assets/cbs_logo.png")}></Image>
-    </View>
-    </View>
-  
-  );
+				<Button title="Register" style={styles.button} onPress={register} />
+				<Image source={require("../assets/cbs_logo.png")}></Image>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  button: {
-    width: 200,
-    marginTop: 10,
-  },
-  container: {
-    marginTop: 70,
-    flex: 2,
-    alignItems: "center",
-    padding: 10,
-    backgroundColor: "#FFFFFF",
-  },
-  parentContainer: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-  }
+	button: {
+		width: 200,
+		marginTop: 10,
+	},
+	container: {
+		marginTop: 70,
+		flex: 2,
+		alignItems: "center",
+		padding: 10,
+		backgroundColor: "#FFFFFF",
+	},
+	parentContainer: {
+		flex: 1,
+		backgroundColor: "#FFFFFF",
+	},
 });
 
 export default RegisterScreen;
